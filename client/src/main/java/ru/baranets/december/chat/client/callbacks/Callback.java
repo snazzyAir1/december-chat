e05390e4ff3a2fc117255e836c010934e90bb062
{nazar} <{nazar.baranec@mail.ru}>
2023-12-23 00:21:03 +0300

    выполнил домашнее задание номер 22.

---
+++ client/src/main/java/ru/baranets/december/chat/client/callbacks/Callback.java
@@ -0,0 +1,5 @@
+package ru.baranets.december.chat.client.callbacks;
+
+public interface Callback {
+    void callback(Object... args);
+}
---
+++ server/src/main/java/ServerApplication.java
@@ -0,0 +1,6 @@
+public class ServerApplication {
+    public static void main(String[] args) {
+        Server server = new Server(8189);
+        server.start();
+    }
+}
